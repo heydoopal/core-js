7a8db94daebfb27abc664237cb9ccb554ec296ad
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-06-25 10:19:24 +0900

    feat: learn. jujeob generator - showAlert - shake

--- client/lib/dom/index.js
+++ client/lib/dom/index.js
@@ -1,27 +1,14 @@
 
 
 
-// attr 함수를 받아옴
 // 다시 내보내기 re-export
 
 
-
+export * from './css.js';
 export * from './attr.js';
+export * from './insert.js';
+export * from './getNode.js';
+export * from './showAlert.js';
 export * from './bindEvent.js';
 export * from './clearContents.js';
 // export {default as clearContents} from './clearContents.js';
-export * from './css.js';
-export * from './getNode.js';
-export * from './insert.js';
-
-
-
-
-
-
-
-
-
-
-
-
--- client/main.js
+++ client/main.js
@@ -1,15 +1,16 @@
-
-import data from "./data/data.js";
+import data from './data/data.js';
 import { 
+  shake, 
   getNode, 
+  addClass,
+  showAlert,
   getRandom, 
   insertLast, 
-  clearContents 
-} from './lib/index.js'
-
-
-
-
+  removeClass,
+  clearContents,
+  isNumericString,
+  copy
+ } from './lib/index.js';
 
 // [phase-1]
 // 1. 주접 떨기 버튼을 클릭 하는 함수
@@ -25,25 +26,59 @@ import {
 
 // 4. pick 항목 랜더링하기
 
+// [phase-2]
+// 1. 아무 값도 입력 받지 못했을 때 예외처리 (콘솔 출력)
 
 const submit = getNode('#submit');
 const nameField = getNode('#nameField');
 const result = getNode('.result');
 
 
-function handleSubmit(e){
+
+function handleSubmit(e) {
   e.preventDefault();
 
   const name = nameField.value;
   const list = data(name);
   const pick = list[getRandom(list.length)];
 
-  console.log( pick );
-  
-  clearContents(result);
-  insertLast(result,pick);
 
+  if (!name || name.replace(/\s*/g,'') === '') {
+    
+    showAlert('.alert-error','공백은 허용하지 않습니다.')
+    
+    shake('#nameField').restart();
+
+    return;
+  }
+
+
+  
+  if(!isNumericString(name)){
+    
+    showAlert('.alert-error','제대로된 이름을 입력해 주세요.');
+
+    shake('#nameField').restart();
+
+    return;
+  }
+
+  clearContents(result);
+  insertLast(result, pick);
+}
+
+
+
+function handleCopy(){
+  const text = result.textContent;
+
+  if(nameField.value){
+
+    copy(showAlert('.alert-success','클립보드 복사 완료!'))
+
+  }
   
 }
 
-submit.addEventListener('click',handleSubmit)
+submit.addEventListener('click', handleSubmit);
+result.addEventListener('click', handleCopy);
