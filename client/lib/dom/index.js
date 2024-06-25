d968342e072c8530407af7d613df6ce48cd50e46
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-06-25 17:15:12 +0900

    learn. randomDice

--- client/lib/dom/index.js
+++ client/lib/dom/index.js
@@ -1,15 +1,11 @@
-
-
-
 // 다시 내보내기 re-export
 
-
-export * from './css.js';
 export * from './attr.js';
-export * from './insert.js';
-export * from './getNode.js';
-export * from './showAlert.js';
 export * from './bindEvent.js';
-export * from './endScroll.js'
 export * from './clearContents.js';
+export * from './css.js';
+export * from './endScroll.js';
+export * from './getNode.js';
+export * from './insert.js';
+export * from './showAlert.js';
 // export {default as clearContents} from './clearContents.js';
--- client/main.js
+++ client/main.js
@@ -1,6 +1,4 @@
-
-
-import { 
+import {
   attr,
   getNode,
   getNodes,
@@ -8,91 +6,75 @@ import {
   insertLast,
   diceAnimation,
   clearContents,
- } from "./lib/index.js";
-
+} from './lib/index.js';
 
 // 1. 주사위 애니메이션
 // 2. 주사위 굴리기 버튼을 클릭하면 diceAnimation() 실행될 수 있도록
 
-
-
-
-const [rollingButton,recordButton,resetButton] = getNodes('.buttonGroup > button');
+const [rollingButton, recordButton, resetButton] = getNodes(
+  '.buttonGroup > button'
+);
 const recordListWrapper = getNode('.recordListWrapper');
-
 
 let count = 0;
 let total = 0;
 
-
-function createItem(value){
+function createItem(value) {
   const template = `
     <tr>
       <td>${++count}</td>
       <td>${value}</td>
-      <td>${total += value}</td>
+      <td>${(total += value)}</td>
     </tr>
-  `
-  return template
+  `;
+  return template;
 }
 
-
-function renderRecordItem(){
-  
+function renderRecordItem() {
   // const diceValue = getNode('#cube').getAttribute('dice');
-  const diceValue = Number(attr(getNode('#cube'),'dice'));
+  const diceValue = Number(attr(getNode('#cube'), 'dice'));
 
+  insertLast('.recordList tbody', createItem(diceValue));
 
-  insertLast('.recordList tbody',createItem(diceValue))
-  
   // 1. insertLast 함수 사용
 
   // 2. template 전달
 
   // 3. diceValue interpolation(보간법) 하기 ex) <td>${diceValue}</td>
   endScroll(recordListWrapper);
-
 }
 
-
-
 const handleRollingDice = (() => {
-
-  
   let isClicked = false;
   let stopAnimation;
 
-  return ()=>{
-    if(!isClicked){
-      stopAnimation  = setInterval(diceAnimation, 100);
+  return () => {
+    if (!isClicked) {
+      stopAnimation = setInterval(diceAnimation, 100);
       recordButton.disabled = true;
       resetButton.disabled = true;
-    }
-    else{
+    } else {
       clearInterval(stopAnimation);
       recordButton.disabled = false;
       resetButton.disabled = false;
     }
     isClicked = !isClicked;
-  }
-})()
+  };
+})();
 
-
-function handleRecord(){
+function handleRecord() {
   recordListWrapper.hidden = false;
 
-  renderRecordItem()
+  renderRecordItem();
 }
 
-
-function handleReset(){
+function handleReset() {
   recordListWrapper.hidden = true;
   clearContents('tbody');
   count = 0;
   total = 0;
-  
 }
 
-rollingButton.addEventListener('click',handleRollingDice)
-recordButton.addEventListener('click',handleRecord)
-resetButton.addEventListener('click',handleReset)
+rollingButton.addEventListener('click', handleRollingDice);
+recordButton.addEventListener('click', handleRecord);
+resetButton.addEventListener('click', handleReset);
